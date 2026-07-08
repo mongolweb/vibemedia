f4505f7902008938b5584a29bb0b302319152e49
mongolweb <mailteihvn@gmail.com>
2026-07-08 23:56:46 +0800

    Update Pricing.jsx

--- vmedia.mn/components/Pricing.jsx
+++ vmedia.mn/components/Pricing.jsx
@@ -11,8 +11,8 @@ const team = [
   },
   {
     id: 2,
-    name: "С.Мөнх-Оргил",
-    role: "Full Stack Developer",
+    name: "Б.Баатархүү",
+    role: "Founder & Co-CEO",
     image: "/team/Orgil.jpg",
   },
   {
